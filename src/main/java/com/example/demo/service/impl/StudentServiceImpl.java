fa78a6e6883dd2d59379619ae99072ee51d7543b
bala-amypo <bala@amypo.in>
2025-12-17 06:21:38 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -5,5 +5,25 @@ import com.example.demo.entity.Student;
 
 @Service
 public class StudentServiceImpl implements StudentService{
-    private final Map<Long,Student
+    private final Map<Long,Student> store=new HashMap<>();
+    private long counter=1;
+
+    @Override
+    public Student insertStudent(Student st){
+        st.setId(counter++);
+        store.put(set.getId(),st);
+        return st;
+    }
+    @Override
+    public List<Student> getAllStudent(){
+        return new ArrayList<>(store.values());
+    }
+    @Override
+    public Optional<Student> getOneStudent(Long id){
+        return Optional.ofNullable(store.get(id));
+    }
+    @Override
+    public void deleteStudent(Long id){
+        store.remove(id);
+    }
 }
