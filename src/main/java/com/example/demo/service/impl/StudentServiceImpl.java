b85277c094bffdeaeeb31d47bebb043a88d02432
bala-amypo <bala@amypo.in>
2025-12-17 06:16:38 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -1,5 +1,9 @@
-StudentRepo.java
-package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.demo.entity
+package com.example.demo.service.Impl;
+import java.util.*;
+import org.springframework.sterotype.Service;
+import com.example.demo.entity.Student;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+    private final Map<Long,Student
+}
