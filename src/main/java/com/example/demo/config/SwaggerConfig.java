e566309d7d8b3ee6f15ab05cfc4b5b9774f77fd5
bala-amypo <bala@amypo.in>
2025-12-18 05:28:43 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,22 +1,19 @@
-
-
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI openAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9164.pro604cr.amypo.ai/")
-                ));
-        }
+                .info(new Info()
+                        .title("Transport API - User & Vehicle CRUD")
+                        .version("1.0.0")
+                        .description("CRUD endpoints for User and Vehicle"));
+    }
 }
